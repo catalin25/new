f9ebb20b0f10f8f572b528030bd7b5b8c54ddbb4
catalin25 <catalin.tudoreanu1@gmail.com>
2022-10-23 21:19:13 +0300

    Add files via upload

---
+++ assets.js
@@ -0,0 +1,15 @@
+export default function loadAssets() {
+	loadSprite("bean", "sprites/bean.png");
+	loadSprite("ghosty", "sprites/ghosty.png");
+	loadSprite("spike", "sprites/spike.png");
+	loadSprite("grass", "sprites/grass.png");
+	loadSprite("prize", "sprites/jumpy.png");
+	loadSprite("apple", "sprites/apple.png");
+	loadSprite("portal", "sprites/portal.png");
+	loadSprite("coin", "sprites/coin.png");
+	loadSound("coin", "sounds/score.mp3");
+	loadSound("powerup", "sounds/powerup.mp3");
+	loadSound("blip", "sounds/blip.mp3");
+	loadSound("hit", "sounds/hit.mp3");
+	loadSound("portal", "sounds/portal.mp3");
+}
